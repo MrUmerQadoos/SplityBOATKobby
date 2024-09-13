758a272b2cc59651f9155fd036d713443a519125
Umer Qadoos <94630047+MrUmerQadoos@users.noreply.github.com>
2024-09-13 15:08:02 -0700

    client side

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,60 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  swcMinify: true, // Enable this to use faster SWC compiler minification
+
+  // Increase API Route Payload Size (if needed)
+  api: {
+    bodyParser: {
+      sizeLimit: '100mb', // Increase body size limit for large uploads
+    },
+  },
+
+  // Add headers if necessary (for CORS or security)
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Apply to all routes
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Adjust CORS as per your need
+          },
+        ],
+      },
+    ];
+  },
 
   async redirects() {
     return [
       {
         source: '/',
         destination: '/login',
-        permanent: false
-      }
-    ]
+        permanent: false,
+      },
+    ];
   }
 }
 
-export default nextConfig
+export default nextConfig;
+
+
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+
+
+//   reactStrictMode: false,
+
+//   async redirects() {
+//     return [
+//       {
+//         source: '/',
+//         destination: '/login',
+//         permanent: false
+//       }
+//     ]
+//   }
+// }
+
+// export default nextConfig
